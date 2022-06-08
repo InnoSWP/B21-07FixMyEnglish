0f7fdd9c9a9a53ce84f2898ff26639f9ed91f59d
SokolOFFF <egor1sokolov@gmail.com>
2022-06-08 22:26:41 +0300

    copied page for upload button

---
+++ lib/FRONT-END/MainPageForAnalysisPdfFiles/ReportPartWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ReportPartWidget extends StatelessWidget {
+  List<String> list;
+  ReportPartWidget(this.list, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [],);
+  }
+}
