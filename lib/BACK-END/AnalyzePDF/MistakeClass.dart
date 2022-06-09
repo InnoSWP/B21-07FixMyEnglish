6b9cd97fe1cd133cd7029c504f3c3c93a7f46715
SokolOFFF <egor1sokolov@gmail.com>
2022-06-09 23:18:21 +0300

    implemented mistake class

--- lib/BACK-END/AnalyzePDF/MistakeClass.dart
+++ lib/BACK-END/AnalyzePDF/MistakeClass.dart
@@ -1,3 +1,13 @@
 class Mistake{
-  // TODO: implement it: properties and values
+  late String wrong_phrase;
+  late String sentence;
+  late String label;
+  late String description;
+
+  Mistake.fromJson(Map<String, dynamic> json) {
+    wrong_phrase = json['match'];
+    sentence = json['sentence'];
+    label = json['label'];
+    description = json['description'];
+  }
 }
