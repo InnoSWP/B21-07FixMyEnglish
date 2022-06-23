a06ea8ef33d5b6b30948ec9ffef364e55d4b87e8
SokolOFFF <egor1sokolov@gmail.com>
2022-06-24 00:58:15 +0300

    implemented uploading test

--- test/uploading_files_test.dart
+++ test/uploading_files_test.dart
@@ -1,35 +1,33 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
 import 'dart:typed_data';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart';
-import 'package:syncfusion_flutter_pdf/pdf.dart';
-import 'package:web1_app/BACK-END/AnalyzePDF/AnalyzePDF.dart';
 import 'package:web1_app/BACK-END/DefaultFileClass.dart';
-import 'package:web1_app/BACK-END/AnalyzePDF/SentencePartClass.dart';
 import 'package:web1_app/BACK-END/AnalyzePDF/UploadPDF.dart';
-import 'package:web1_app/BACK-END/Exporting/ExportFile.dart';
-import 'package:test/test.dart';
-import 'package:path/path.dart';
 
-Future<void> main() async {
-  List<DefaultFile> files = [];
-
-  var data1 = await rootBundle.load('assets/test_file_1.pdf');
-  Uint8List myBytes1 = data1.buffer.asUint8List(data1.offsetInBytes, data1.lengthInBytes);
-
-  var data2 = await rootBundle.load('assets/test_file_2.pdf');
-  Uint8List myBytes2 = data2.buffer.asUint8List(data2.offsetInBytes, data2.lengthInBytes);
-
+void main() {
   group('Uploading tests', ()
   {
-    test('Get text from file 1', () async{
-      files.add(DefaultFile(myBytes1, "test_file_1.pdf"));
-      expect(PdfAPI.getFilesTexts(files)![0].text, "There aren’t any problem with that. I have to use 8 tools to modify this problem. I broke my car, that’s pitty.");
+    List<DefaultFile> files = [];
+
+    testWidgets('Get text from file 1', (WidgetTester tester) async {
+      tester.runAsync(() async {
+        var file = File('assets/test_file_1.pdf');
+        final Uint8List encoded = utf8.encoder.convert(file.readAsStringSync());
+        files.add(DefaultFile(encoded, "test_file_1.pdf"));
+        expect(PdfAPI.getFilesTexts(files)![0].text,
+            "There aren’t any problem with that. I have to use 8 tools to modify this problem. I broke my car, that’s pitty.");
+      });
     });
-    test('Get text from file 2', () async{
-      files.clear();
-      files.add(DefaultFile(myBytes2, 'test_file_2.pdf'));
-      expect(PdfAPI.getFilesTexts(files)![0].text, "Hello, I’m in love with Flutter. Bring my tea, please. Be careful: it’s hot.");
+
+    testWidgets('Get text from file 2', (WidgetTester tester) async {
+      tester.runAsync(() async {
+        var file = File('assets/test_file_2.pdf');
+        final Uint8List encoded = utf8.encoder.convert(file.readAsStringSync());
+        files.clear();
+        files.add(DefaultFile(encoded, "test_file_2.pdf"));
+        expect(PdfAPI.getFilesTexts(files)![0].text, "Hello, I’m in love with Flutter. Bring my tea, please. Be careful: it’s hot.");
+      });
     });
   });
 }
